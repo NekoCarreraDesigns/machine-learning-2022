f8093959ca673184104c8823853e76d66accd773
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-30 12:58:46 -0600

    renamed file, added functions to save and read json

--- multiclipboard.py
+++ multiclipboard.py
@@ -3,6 +3,17 @@ import clipboard
 import json
 
 
+def save_items(filepath, data):
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
+
+
+def load_items(filepath):
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+        return data
+
+
 if len(sys.argv) == 2:
     command = sys.argv[1]
 
