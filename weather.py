5bc5a9bbb6d6c1a2db98f4f4c21670d4c755bdfb
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-31 12:06:25 -0600

    added code for weather fetching

--- weather.py
+++ weather.py
@@ -1,2 +1,18 @@
+from wsgiref.util import request_uri
+import requests
 
 API_KEY = 'a8873d0d448c9dc93f60cba6ef75cb3e'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+city = input('enter a city name: ')
+request_url = f'{BASE_URL}?appid={API_KEY}&q={city}'
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data['main']['temp'] - 273.15, 2)
+    print('weather:', weather)
+    print('temperature:', temperature, 'celsius')
+else:
+    print('error')
